9a7f1147fcc5ce4b78475fa2d2ea640bf6d16bdf
Dilik_kun <dilik.rayli@gmail.com>
2024-04-06 13:57:56 +0500

    function created

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+def read_recipes(filename):
+    cook_book = {}
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            dish_name = line.strip()
+            num_ingredients = int(file.readline())
+            ingredients_list = []
+            for _ in range(num_ingredients):
+                ingredient_name, quantity, measure = file.readline().split(' | ')
+                ingredients_list.append({'ingredient_name': ingredient_name, 'quantity': int(quantity), 'measure': measure.strip()})
+            cook_book[dish_name] = ingredients_list
+            file.readline()  # пропускаем пустую строку
+    return cook_book
 
+cook_book = read_recipes('receipts.txt')
+print(cook_book)
